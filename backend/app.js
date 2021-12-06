8fd7c36b2adf608f7c8131ff08f487a93613e4ac
VANG Robert <robert.vang@hotmail.fr>
2021-12-06 13:56:39 +0100

    ajout de plugins de securité: limit, helmet, .env

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const helmet = require("helmet");
+
+require('dotenv').config()
 
 const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
@@ -16,7 +19,9 @@ app.use((req, res, next) => {
     next();
   });
 
-mongoose.connect('mongodb+srv://RVANG:321654987@cluster0.iiubf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+  app.use(helmet());
+
+mongoose.connect(process.env.DB_URL,
 { useNewUrlParser: true, useUnifiedTopology: true })
 .then(() => console.log('Connexion à MongoDB réussie !'))
 .catch(() => console.log('Connexion à MongoDB échouée !'));
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userCtrl = require('../controllers/user');
+const limit = require('../middleware/limit');
 
 router.post('/signup', userCtrl.signup);
-router.post('/login', userCtrl.login);
+router.post('/login', limit, userCtrl.login);
 
 module.exports = router;
